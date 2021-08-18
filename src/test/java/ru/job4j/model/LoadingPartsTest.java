3395a18f090e891dcd2e3cf13d96179fd3131f9a
konstantin <kotmix@bk.ru>
2021-08-18 09:48:53 +0400

    2.3. Загрузка деталей поста. [#285212]

---
+++ src/test/java/ru/job4j/model/LoadingPartsTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.model;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LoadingPartsTest {
+    @Test
+    public void whenLinkThenPost() throws IOException {
+        LoadingParts load = new LoadingParts();
+        String link = "https://www.sql.ru/forum/1325330/"
+                + "lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
+        Post expected = load.loading(link);
+        Post result = new Post(
+                "Лиды BE/FE/senior cистемные аналитики/QA и DevOps, Москва, до 200т.",
+                link,
+                "Ищем команды разработчиков на аутстафф в офис в Москве, а также"
+                        + " тимлидов и ведущих системных аналитиков, QA и DevOps Зп до 200т."
+                        + " Наши приоритеты стаффинга: 1. Команды, микрокоманды: - команда -"
+                        + " 7-9 человек (типовой состав - 2 аналитика, 3 Java, 2 React, 2 QA);"
+                        + " - микрокоманда - 3-4 человека (специальность - BE/FE/QA/QA"
+                        + " автотестирование). Ключевые специалисты команды должны сделать"
+                        + " минимум 1 проект вместе. 2. Лиды BE/FE/senior cистемные"
+                        + " аналитики/QA и DevOps. Наш стек: Backend:"
+                        + " java/kotlin, spring Frontend: react Mobile: kotlin/Android, Swift/iOS"
+                        + " Предложения и резюме присылайте на contact.softlantic@gmail.com",
+                LocalDateTime.of(2020, 05, 13, 21, 58)
+        );
+        assertThat(result, is(expected));
+    }
+}
