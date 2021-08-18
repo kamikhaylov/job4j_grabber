157d3ecdd4988e18d6225e5e3874f45c5dad0d10
konstantin <kotmix@bk.ru>
2021-08-18 22:59:03 +0400

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359]

---
+++ src/main/java/ru/job4j/store/Store.java
@@ -0,0 +1,12 @@
+package ru.job4j.store;
+
+import ru.job4j.model.Post;
+import java.util.List;
+
+public interface Store {
+    void save(Post post);
+
+    List<Post> getAll();
+
+    Post findById(int id);
+}
